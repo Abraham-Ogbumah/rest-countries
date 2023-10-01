68a0426aab65edd74ec800266ffd0196b923b2bc
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2023-10-01 23:30:59 +0100

    update to various files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaRegMoon } from 'react-icons/fa';
+import ThemeContext  from "../context/ThemeContext";
+
 
 function Header() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
     return (
         <div className="header center-items">
             <div className="navText">
                 <p>Where in the world?</p>
             </div>
 
-            <div className="themeSwitcher">
+            <div className="themeSwitcher" onClick={toggleTheme}>
                 <div>
                      <FaRegMoon />
                 </div>
                 <div>
-                     <p>Dark Mode</p> 
+                     <p>{theme === "light" ? "Dark Mode" : "Light Mode"} </p> 
                 </div>
             </div>
         </div>
